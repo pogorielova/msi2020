be49d8810a55925b948f916ab601772b9a32b6d1
Pogorielova Anastasia <nastasia@pogorielovasmbp.lan>
2020-06-29 20:37:46 +0300

    Updates

---
+++ precache-manifest.e7302ba2396b06b915adce328d62bed1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8407a011a006087fb0358dffb3ae8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a380fbb7d74bb40ea88",
+    "url": "/static/css/main.37293361.chunk.css"
+  },
+  {
+    "revision": "db95e4ef000d4d16dacb",
+    "url": "/static/js/2.d8385d09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8385d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a380fbb7d74bb40ea88",
+    "url": "/static/js/main.c8ed973e.chunk.js"
+  },
+  {
+    "revision": "20fe4ec334c80b513d28",
+    "url": "/static/js/runtime-main.3282b8e5.js"
+  },
+  {
+    "revision": "dbd9017e3a6d90935746162a2a72431f",
+    "url": "/static/media/fvrt-close.dbd9017e.svg"
+  },
+  {
+    "revision": "358f75fed4bb54fe11cd68ab147b7c8d",
+    "url": "/static/media/fvrt-open.358f75fe.svg"
+  },
+  {
+    "revision": "e977fb9e726c00e0765ae53cc9a0dd66",
+    "url": "/static/media/heart-empty.e977fb9e.svg"
+  },
+  {
+    "revision": "0865c8322b3cc6f1bed5a8f1f35f00b5",
+    "url": "/static/media/heart-filled.0865c832.svg"
+  },
+  {
+    "revision": "f8a998d1878c0acd21eab998d9eead5c",
+    "url": "/static/media/link-icon.f8a998d1.svg"
+  },
+  {
+    "revision": "ee31211cb3e3b4e4c50c3d68317da746",
+    "url": "/static/media/message-icon.ee31211c.svg"
+  }
+]);
